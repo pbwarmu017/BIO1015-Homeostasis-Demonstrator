0f4a477793088aaf7071a6b4e11f946d2625cfcc
Paul Warmuth <warmuth.paul@gmail.com>
2020-01-07 20:48:41 -0600

    OMGNO

--- multi-effector_homeostasis_device/includes.h
+++
@@ -1,9 +0,0 @@
-//class forward declarations
-
-//main pin defines
-#include <Arduino.h>
-//neopixels
-#include <Adafruit_NeoPixel.h>
-//RGB LCD Display
-#include <Wire.h>
-#include <Adafruit_RGBLCDShield.h>
--- multi-effector_homeostasis_device/multi-effector_homeostasis_device.ino
+++ multi-effector_homeostasis_device/multi-effector_homeostasis_device.ino
@@ -3,8 +3,67 @@ REQUIRED LIBRARIES: Must be installed in the arduino IDE to compile this
 Adafruit_Neopixel
 Adafruit RGB LCD Sheild Library
 */
-#include "includes.h"
-#include "multi-effector_homeostasis_device.h"
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <Adafruit_RGBLCDShield.h>
+
+
+enum GAMESTATUS {
+  notstarted,
+  started,
+  lost
+};
+
+enum SYSTEMMODE {
+  calibrate,
+  running,
+  config
+};
+
+//port number defines for readability 
+#define DCON1_PORTNUM 1
+#define ACON1_PORTNUM 2
+#define DACON1_PORTNUM 3
+#define DCON2_PORTNUM 4
+#define ACON2_PORTNUM 5
+#define DACON2_PORTNUM 6
+#define HARDCODED_PORTNUM 7 //this is used when the port is hardcoded in the library (lcd)
+
+#define indicatorStrip_type 1
+#define handGrip_type 2
+#define handCrank_type 3
+#define lcd_type 4
+#define menu_type 5
+
+extern enum GAMESTATUS gameStatus;
+extern enum SYSTEMMODE systemMode;
+
+#define HANDGRIPPIN A0
+#define ENCODERPINA 3
+#define ENCODERPINB 5
+
+#define HANDGRIPDEVNUM 0 //device number for handgrip
+#define CRANKDEVNUM 1
+
+#define HANDGRIPACTIVE 1
+#define CRANKACTIVE 1
+
+#define STRIPTESTMODE 0 //used for internal testing
+
+#define GAMERESETDELAY 5000 //in milliseconds
+
+#define LCDREFRESHDELAY 205 //in milliseconds
+
+#define SELECTTIMERROLLOVER 5
+
+#define RED 0x1
+#define YELLOW 0x3
+#define GREEN 0x2
+#define TEAL 0x6
+#define BLUE 0x4
+#define VIOLET 0x5
+#define WHITE 0x7
 
 //LEDSTRIP
 #define LED_PIN 6
@@ -24,7 +83,7 @@ Adafruit RGB LCD Sheild Library
 //AFFECTOR INDICATOR COLOR SELECTIONS
 #define SQUEEZEINDICATORCOLOR COLORBLUE
 #define CRANKINDICATORCOLOR COLORORANGE
- //DEFAULT RATES
+//DEFAULT RATES
 #define DEFAULTINDICATORPOSITION 0//BOXSTART+round((float)BOXSIZE/2)
 #define DEFAULTPRODUCTIONRATE 25 
 #define DEFAULTCONSUMPTIONRATE 50
