d1ee3578c575085c45f80858c236ee10ca7593ec
Paul Warmuth <warmuth.paul@gmail.com>
2020-01-19 12:22:03 -0600

    Brought back color codes

--- multi-effector_homeostasis_device/lcd.cpp
+++ multi-effector_homeostasis_device/lcd.cpp
@@ -1,6 +1,15 @@
 #ifndef LCD_CPP
 #define LCD_CPP
 #include "superclasses.cpp"
+ //LCD background color defines
+#define RED 0x1
+#define YELLOW 0x3
+#define GREEN 0x2
+#define TEAL 0x6
+#define BLUE 0x4
+#define VIOLET 0x5
+#define WHITE 0x7
+
 class _lcd: public _device {
   public: 
     Adafruit_RGBLCDShield *lcd_obj;
