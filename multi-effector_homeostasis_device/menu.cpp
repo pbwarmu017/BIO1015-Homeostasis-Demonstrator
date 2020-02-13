a55eff678541f746c0ef0d06b4f15a4b773a4af7
Paul Warmuth <warmuth.paul@gmail.com>
2020-02-12 20:59:28 -0600

    Formatting

--- multi-effector_homeostasis_device/menu.cpp
+++ multi-effector_homeostasis_device/menu.cpp
@@ -5,128 +5,129 @@
 #include "lcd.cpp"
 #include "multi-effector_homeostasis_device.h"
 
-#define MAXIMUMDEVICES 2 //defines the max number of devices the system will allow to connect. 
+//DEFINES
+  #define MAXIMUMDEVICES 2 //defines the max number of devices the system will allow to connect. 
 
-//DEVICE STATES (used to track port status. As you add affectors and use these states, add descriptions here)
-//note: there can only be 16 port states, as I am using only 4 bits to track it. 
-#define DCONSTATE0 0 //OFF
-#define DCONSTATE1 1 //HANDCRANK
-#define DCONSTATE2 2
-#define DCONSTATE3 3
-#define DCONSTATE4 4
-#define DCONSTATE5 5
-#define DCONSTATE6 6
-#define DCONSTATE7 7
-#define DCONSTATE8 8
-#define DCONSTATE9 9
-#define DCONSTATE10 10
-#define DCONSTATE11 11
-#define DCONSTATE12 12
-#define DCONSTATE13 13
-#define DCONSTATE14 14
-#define DCONSTATE15 15 //uninitialized
+  //DEVICE STATES (used to track port status. As you add affectors and use these states, add descriptions here)
+  //note: there can only be 16 port states, as I am using only 4 bits to track it. 
+  #define DCONSTATE0 0 //OFF
+  #define DCONSTATE1 1 //HANDCRANK
+  #define DCONSTATE2 2
+  #define DCONSTATE3 3
+  #define DCONSTATE4 4
+  #define DCONSTATE5 5
+  #define DCONSTATE6 6
+  #define DCONSTATE7 7
+  #define DCONSTATE8 8
+  #define DCONSTATE9 9
+  #define DCONSTATE10 10
+  #define DCONSTATE11 11
+  #define DCONSTATE12 12
+  #define DCONSTATE13 13
+  #define DCONSTATE14 14
+  #define DCONSTATE15 15 //uninitialized
 
 
-#define ACONSTATE0 0 //OFF
-#define ACONSTATE1 1 //HANDGRIP
-#define ACONSTATE2 2
-#define ACONSTATE3 3
-#define ACONSTATE4 4
-#define ACONSTATE5 5
-#define ACONSTATE6 6
-#define ACONSTATE7 7
-#define ACONSTATE8 8
-#define ACONSTATE9 9
-#define ACONSTATE10 10
-#define ACONSTATE11 11
-#define ACONSTATE12 12
-#define ACONSTATE13 13
-#define ACONSTATE14 14
-#define ACONSTATE15 15 //uninitialized
+  #define ACONSTATE0 0 //OFF
+  #define ACONSTATE1 1 //HANDGRIP
+  #define ACONSTATE2 2
+  #define ACONSTATE3 3
+  #define ACONSTATE4 4
+  #define ACONSTATE5 5
+  #define ACONSTATE6 6
+  #define ACONSTATE7 7
+  #define ACONSTATE8 8
+  #define ACONSTATE9 9
+  #define ACONSTATE10 10
+  #define ACONSTATE11 11
+  #define ACONSTATE12 12
+  #define ACONSTATE13 13
+  #define ACONSTATE14 14
+  #define ACONSTATE15 15 //uninitialized
 
-#define DACONSTATE0 0 //OFF
-#define DACONSTATE1 1
-#define DACONSTATE2 2
-#define DACONSTATE3 3
-#define DACONSTATE4 4
-#define DACONSTATE5 5
-#define DACONSTATE6 6
-#define DACONSTATE7 7
-#define DACONSTATE8 8
-#define DACONSTATE9 9
-#define DACONSTATE10 10
-#define DACONSTATE11 11
-#define DACONSTATE12 12
-#define DACONSTATE13 13
-#define DACONSTATE14 14
-#define DACONSTATE15 15 //uninitialized 
+  #define DACONSTATE0 0 //OFF
+  #define DACONSTATE1 1
+  #define DACONSTATE2 2
+  #define DACONSTATE3 3
+  #define DACONSTATE4 4
+  #define DACONSTATE5 5
+  #define DACONSTATE6 6
+  #define DACONSTATE7 7
+  #define DACONSTATE8 8
+  #define DACONSTATE9 9
+  #define DACONSTATE10 10
+  #define DACONSTATE11 11
+  #define DACONSTATE12 12
+  #define DACONSTATE13 13
+  #define DACONSTATE14 14
+  #define DACONSTATE15 15 //uninitialized 
 
-#define BOUNDINGBOXSTATIONARY 0
-#define BOUNDINGBOXMOVING 1
+  #define BOUNDINGBOXSTATIONARY 0
+  #define BOUNDINGBOXMOVING 1
 
-#define DCON1FLAG 0
-#define DCON2FLAG 1
-#define ACON1FLAG 2
-#define ACON2FLAG 3
-#define DACON1FLAG 4
-#define DACON2FLAG 5
+  #define DCON1FLAG 0
+  #define DCON2FLAG 1
+  #define ACON1FLAG 2
+  #define ACON2FLAG 3
+  #define DACON1FLAG 4
+  #define DACON2FLAG 5
 
-//this is some PROGMEM magic to move the strings to program memory and free up SRAM
 //LCD MENU TEXT OPTIONS
+  //this is some PROGMEM magic to move the strings to program memory and free up SRAM
 
-//Main Menu Screens (line 1)
-#define NUMOFSCREENS 9 //number of options in screens[]
-constexpr const char screen_1_1[17] PROGMEM = "PORT DCON1";
-constexpr const char screen_2_1[17] PROGMEM = "PORT ACON1";
-constexpr const char screen_3_1[17] PROGMEM = "PORT DACON1";
-constexpr const char screen_4_1[17] PROGMEM = "PORT DCON2";
-constexpr const char screen_5_1[17] PROGMEM = "PORT ACON2";
-constexpr const char screen_6_1[17] PROGMEM = "PORT DACON2";
-constexpr const char screen_7_1[17] PROGMEM = "BOUNDING BOX";
-constexpr const char screen_8_1[17] PROGMEM = "BOUNDING BOX";
-constexpr const char screen_9_1[17] PROGMEM = "SOFTWARE VERSION";
-constexpr const char* screen1table[NUMOFSCREENS] PROGMEM = {screen_1_1, screen_2_1, screen_3_1, screen_4_1, 
-  screen_5_1, screen_6_1, screen_7_1, screen_8_1, screen_9_1};
+  //Main Menu Screens (line 1)
+  #define NUMOFSCREENS 9 //number of options in screens[]
+  constexpr const char screen_1_1[17] PROGMEM = "PORT DCON1";
+  constexpr const char screen_2_1[17] PROGMEM = "PORT ACON1";
+  constexpr const char screen_3_1[17] PROGMEM = "PORT DACON1";
+  constexpr const char screen_4_1[17] PROGMEM = "PORT DCON2";
+  constexpr const char screen_5_1[17] PROGMEM = "PORT ACON2";
+  constexpr const char screen_6_1[17] PROGMEM = "PORT DACON2";
+  constexpr const char screen_7_1[17] PROGMEM = "BOUNDING BOX";
+  constexpr const char screen_8_1[17] PROGMEM = "BOUNDING BOX";
+  constexpr const char screen_9_1[17] PROGMEM = "SOFTWARE VERSION";
+  constexpr const char* screen1table[NUMOFSCREENS] PROGMEM = {screen_1_1, screen_2_1, screen_3_1, screen_4_1, 
+    screen_5_1, screen_6_1, screen_7_1, screen_8_1, screen_9_1};
 
-//Main Menu Screens (line 2)
-constexpr const char screen_1_2[17] PROGMEM = "DEV: ";
-constexpr const char screen_2_2[17] PROGMEM = "DEV: ";
-constexpr const char screen_3_2[17] PROGMEM = "DEV: ";
-constexpr const char screen_4_2[17] PROGMEM = "DEV: ";
-constexpr const char screen_5_2[17] PROGMEM = "DEV: ";
-constexpr const char screen_6_2[17] PROGMEM = "DEV: ";
-constexpr const char screen_7_2[17] PROGMEM = "MODE: ";
-constexpr const char screen_8_2[17] PROGMEM = "SIZE: ";
-constexpr const char screen_9_2[17] PROGMEM = "";
-constexpr const char* screen2table[] PROGMEM =  {screen_1_2, screen_2_2, screen_3_2, screen_4_2, screen_5_2, 
-  screen_6_2, screen_7_2, screen_8_2, screen_9_2};
+  //Main Menu Screens (line 2)
+  constexpr const char screen_1_2[17] PROGMEM = "DEV: ";
+  constexpr const char screen_2_2[17] PROGMEM = "DEV: ";
+  constexpr const char screen_3_2[17] PROGMEM = "DEV: ";
+  constexpr const char screen_4_2[17] PROGMEM = "DEV: ";
+  constexpr const char screen_5_2[17] PROGMEM = "DEV: ";
+  constexpr const char screen_6_2[17] PROGMEM = "DEV: ";
+  constexpr const char screen_7_2[17] PROGMEM = "MODE: ";
+  constexpr const char screen_8_2[17] PROGMEM = "SIZE: ";
+  constexpr const char screen_9_2[17] PROGMEM = "";
+  constexpr const char* screen2table[] PROGMEM =  {screen_1_2, screen_2_2, screen_3_2, screen_4_2, screen_5_2, 
+    screen_6_2, screen_7_2, screen_8_2, screen_9_2};
 
-//DCON device options
-#define NUMOFDCONDEVS 2 //number of options in dconscreens[]
-constexpr const char dconscreen_1[17] PROGMEM = {"OFF"};
-constexpr const char dconscreen_2[17] PROGMEM = {"HAND CRANK"};
-constexpr const char* dconscreentable[] PROGMEM = {dconscreen_1, dconscreen_2};
+  //DCON device options
+  #define NUMOFDCONDEVS 2 //number of options in dconscreens[]
+  constexpr const char dconscreen_1[17] PROGMEM = {"OFF"};
+  constexpr const char dconscreen_2[17] PROGMEM = {"HAND CRANK"};
+  constexpr const char* dconscreentable[] PROGMEM = {dconscreen_1, dconscreen_2};
 
-//ACON device options
-#define NUMOFACONDEVS 2 //number of options in aconscreens[]
-constexpr const char aconscreen_1[17] PROGMEM = {"OFF"};
-constexpr const char aconscreen_2[17] PROGMEM = {"SQUEEZE GRIP"};
-constexpr const char* aconscreentable[] PROGMEM = {aconscreen_1, aconscreen_2};
+  //ACON device options
+  #define NUMOFACONDEVS 2 //number of options in aconscreens[]
+  constexpr const char aconscreen_1[17] PROGMEM = {"OFF"};
+  constexpr const char aconscreen_2[17] PROGMEM = {"SQUEEZE GRIP"};
+  constexpr const char* aconscreentable[] PROGMEM = {aconscreen_1, aconscreen_2};
 
-//DACON device options
-#define NUMOFDACONDEVS 1 //number of options in daconscreens[]
-constexpr const char daconscreen_1[17] PROGMEM = {"N/A"};
-constexpr const char* daconscreentable[] PROGMEM = {daconscreen_1};
+  //DACON device options
+  #define NUMOFDACONDEVS 1 //number of options in daconscreens[]
+  constexpr const char daconscreen_1[17] PROGMEM = {"N/A"};
+  constexpr const char* daconscreentable[] PROGMEM = {daconscreen_1};
 
-//BOUNDING BOX MODE OPTIONS
-#define NUMOFBOUNDINGBOXMODES 5 //number of options in boundingBoxModeScreens
-constexpr const char boundingBoxModeScreens_1[17] PROGMEM = {"FROZEN"};
-constexpr const char boundingBoxModeScreens_2[17] PROGMEM = {"MOVING 1X"};
-constexpr const char boundingBoxModeScreens_3[17] PROGMEM = {"MOVING 2X"};
-constexpr const char boundingBoxModeScreens_4[17] PROGMEM = {"MOVING 3X"};
-constexpr const char boundingBoxModeScreens_5[17] PROGMEM = {"MOVING 4X"};
-constexpr const char* boundingBoxModeScreensTable[] PROGMEM = {boundingBoxModeScreens_1, boundingBoxModeScreens_2, 
-  boundingBoxModeScreens_3, boundingBoxModeScreens_4, boundingBoxModeScreens_5};
+  //BOUNDING BOX MODE OPTIONS
+  #define NUMOFBOUNDINGBOXMODES 5 //number of options in boundingBoxModeScreens
+  constexpr const char boundingBoxModeScreens_1[17] PROGMEM = {"FROZEN"};
+  constexpr const char boundingBoxModeScreens_2[17] PROGMEM = {"MOVING 1X"};
+  constexpr const char boundingBoxModeScreens_3[17] PROGMEM = {"MOVING 2X"};
+  constexpr const char boundingBoxModeScreens_4[17] PROGMEM = {"MOVING 3X"};
+  constexpr const char boundingBoxModeScreens_5[17] PROGMEM = {"MOVING 4X"};
+  constexpr const char* boundingBoxModeScreensTable[] PROGMEM = {boundingBoxModeScreens_1, boundingBoxModeScreens_2, 
+    boundingBoxModeScreens_3, boundingBoxModeScreens_4, boundingBoxModeScreens_5};
 
 class _menu: public _device{
   public:
@@ -599,6 +600,7 @@ class _menu: public _device{
         }
         if(currentScreen == 7)
         {
+
           if(boundingboxsize < 25) boundingboxsize++;
         }
         if(currentScreen != 6 && currentScreen != 7) (lcd_ptr->lcd_obj)->setBacklight(TEAL);
